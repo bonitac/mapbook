5c89737dfbac7d0e696bddc02cbaf89572617f57
Bonita Choi <bonitac93@gmail.com>
2019-03-29 17:49:16 +0000

    maps.ejs with sass

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,22 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+    url: "https://maps.googleapis.com/maps/api/place/details/output?parameters"
+  }).done((result)=> console.log(result))
+    // method: "GET",
+    // url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  })
+// });
+
+
+
+
+
+function loginToggle(){
+  $('.login-form').slideToggle('fast');
+  $('#exampleInputEmail1').focus();
+}
